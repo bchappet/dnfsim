41257a1d56b1a84094e800d77431a19d8c1b5313
CARRARA Nicolas <carrara1u@etu.univ-lorraine.fr>
2014-07-04 09:50:52 +0200

    zz

---
+++ src/main/java/network/probalisticFlooding/PFCommandLine.java
@@ -0,0 +1,20 @@
+package main.java.network.probalisticFlooding;
+
+import main.java.network.generic.NetworkCommandLine;
+
+public class PFCommandLine extends NetworkCommandLine{
+	public static String WEIGTH = "w_pf";
+	
+	
+	@Override
+    protected String defaultScript() {
+        return super.defaultScript() + 
+        		/* PFCommandLine */
+                WEIGTH + "=0.0;" +  
+        		TRANSITION_MATRIX_FILE + "=src/main/java/network/ressource/1.pf;";
+        		/* NetworkCommandLine */
+//                NETWORK_DT + "=bd0.1,0.01,1,0.01;";
+    }
+	
+	
+}
