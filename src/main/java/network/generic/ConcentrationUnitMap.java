813c56c92315dc3193388b3a5e59ea3f93e50230
CARRARA Nicolas <carrara1u@etu.univ-lorraine.fr>
2014-08-18 10:48:39 +0200

    e

--- src/main/java/network/generic/ConcentrationUnitMap.java
+++ src/main/java/network/generic/ConcentrationUnitMap.java
@@ -3,14 +3,17 @@ package main.java.network.generic;
 import java.math.BigDecimal;
 
 import main.java.console.CommandLineFormatException;
+import main.java.maps.Parameter;
 import main.java.maps.UnitMap;
 import main.java.maps.Var;
+import main.java.space.Space;
 import main.java.space.Space2D;
+import main.java.unitModel.UnitModel;
 
 public class ConcentrationUnitMap extends UnitMap<Integer,Integer>{
 
 	//String name,Var<BigDecimal> dt,Space<C> space,UnitModel<T> unitModel, Parameter... param
-	public  ConcentrationUnitMap(String name,SpreadingGraph sg,Var<BigDecimal> dt,Var<Integer> size) throws CommandLineFormatException {
+	public ConcentrationUnitMap(String name,SpreadingGraph sg,Var<BigDecimal> dt,Var<Integer> size) throws CommandLineFormatException {
 		super(
 				name, 
 				dt, 
